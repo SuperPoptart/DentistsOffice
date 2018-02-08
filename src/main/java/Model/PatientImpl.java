60c7d640da0e34053a70f71f6a591dc1a2da57b1
Richard Sanchez <superpoptart1@gmail.com>
2018-02-08 13:02:13 -0700

    Made some more stuff

--- src/main/java/Model/PatientImpl.java
+++ src/main/java/Model/PatientImpl.java
@@ -1,6 +1,6 @@
 package Model;
 
-public class PatientImpl extends PersonImpl{
+public class PatientImpl extends PersonImpl {
 
     private String firstName;
     private String lastName;
@@ -8,4 +8,47 @@ public class PatientImpl extends PersonImpl{
     private long phoneNumber;
     private Insurance insurance;
     private String paymentCard;
+
+    public PatientImpl() {
+    }
+
+    public PatientImpl(String fName, String lName, int ID, long pNum, Insurance insurance, String pCard) {
+        super(fName, lName, ID);
+        this.setPhoneNumber(pNum);
+        this.setInsurance(insurance);
+        this.setPaymentCard(pCard);
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        if (phoneNumber < 1000000000) {
+            throw new IllegalArgumentException("Every phone number is 14 digits long and must start with one or greater!");
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(Insurance insurance) {
+        if (insurance == null) {
+            throw new IllegalArgumentException("You must enter an insurance");
+        }
+        this.insurance = insurance;
+    }
+
+    public String getPaymentCard() {
+        return paymentCard;
+    }
+
+    public void setPaymentCard(String paymentCard) {
+        if (paymentCard.length() < 24 || paymentCard.length() > 24) {
+            throw new IllegalArgumentException("You must have a card length that is 24 digits long!");
+        }
+        this.paymentCard = paymentCard;
+    }
 }
