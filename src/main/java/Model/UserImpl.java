f79123b5bcd6e815894f6af246807757cdac6f90
Dcraw139 <DCrawford@student.neumont.edu>
2018-02-08 14:14:51 -0700

    user class done

---
+++ src/main/java/Model/UserImpl.java
@@ -0,0 +1,73 @@
+package Model;
+
+/**
+ * Models a user
+ */
+public class UserImpl {
+    private String username;
+    private String password;
+    private boolean admin;
+
+    public UserImpl() {}
+
+    public UserImpl(String uName, String pWord, boolean admin) {
+        this.setUsername(uName);
+        this.setPassword(pWord);
+        this.setAdmin(admin);
+    }
+
+    /**
+     * gets username
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * sets this username
+     * @param username
+     */
+    public void setUsername(String username) {
+        if(username.equals("") || username == null) {
+            throw new IllegalArgumentException("username must not be null or empty");
+        }
+        this.username = username;
+    }
+
+    /**
+     * gets password
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * sets this password
+     * @param password
+     */
+    public void setPassword(String password) {
+        if(password.equals("") || password == null) {
+            throw new IllegalArgumentException("Password must not be null or empty");
+        }
+        this.password = password;
+    }
+
+    /**
+     * gets Admin
+     * @return admin
+     */
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    /**
+     * sets admin
+     * @param admin
+     */
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+}
