2fbc9b60082fd6ec9590eb2b2a3fdf6e5ceb9f42
Richard Sanchez <superpoptart1@gmail.com>
2018-02-09 13:24:13 -0700

    Made some more stuff

---
+++ src/main/java/DentistApp.java
@@ -0,0 +1,9 @@
+import Controller.DentistManager;
+
+public class DentistApp {
+
+    public static void main(String[] args) {
+        DentistManager dentistManager = new DentistManager();
+        dentistManager.run();
+    }
+}
