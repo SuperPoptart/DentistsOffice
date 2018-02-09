72706fc2da828371269e352f410318df4564fa8d
Richard Sanchez <superpoptart1@gmail.com>
2018-02-09 13:55:16 -0700

    Made some more stuff

--- src/main/java/Controller/DentistManager.java
+++ src/main/java/Controller/DentistManager.java
@@ -17,7 +17,7 @@ public class DentistManager {
     private static final int VIEW_APPOINTMENTS = 4;
     private static final int EXIT = 5;
 
-    public DentistManager() throws IOException{
+    public DentistManager() throws IOException {
         appointment = new Appointment();
         textUI = new TextUI();
 //        this.loadInfo();
@@ -26,16 +26,36 @@ public class DentistManager {
     public void run() throws IOException {
         boolean exitTime = false;
 
-        while (!exitTime){
+        while (!exitTime) {
             int selection = textUI.showMenu(generateMenuOptions());
+            switch (selection) {
+                case EDIT_USERS:
+                    break;
+                case EDIT_PROVIDERS:
+                    break;
+                case EDIT_PATIENTS:
+                    break;
+                case VIEW_APPOINTMENTS:
+                    break;
+                case EXIT:
+//                    this.saveInfo();
+                    exitTime = true;
+                    default:
+
+            }
         }
 
     }
+
+    private static final String filename = "dentistinfo.sav";
 
     private Map<Integer, String> generateMenuOptions() {
         Map<Integer, String> options = new HashMap<>();
         options.put(EDIT_USERS, "Edit Users");
         options.put(EDIT_PROVIDERS, "Edit Providers");
+        options.put(EDIT_PATIENTS, "Edit Patients");
+        options.put(VIEW_APPOINTMENTS, "View Appointments");
+        options.put(EXIT, "Exit");
         return options;
     }
 }
