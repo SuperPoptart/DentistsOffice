70e76e5441708bc4280adce2e738e30e43f5b592
Dcraw139 <DCrawford@student.neumont.edu>
2018-02-09 13:12:16 -0700

    Procedure factory is complete and doc'd

--- src/main/java/Model/ProcedureFactory.java
+++ src/main/java/Model/ProcedureFactory.java
@@ -1,5 +1,30 @@
 package Model;
 
+/**
+ * Creates an instance of Procedure
+ */
 public class ProcedureFactory {
-    //code and stuff
+
+    /**
+     * Default Procedure
+     *
+     * @return procedure
+     */
+    public static Procedure getInstance() {
+        return new ProcedureImpl();
+    }
+
+    /**
+     * Overloaded Procedure
+     *
+     * @param patient  Patient object
+     * @param provider Provider Object
+     * @param code     Procedure code
+     * @param desc     Description of Procedure
+     * @param amount   Cost of Procedure
+     * @return Procedure
+     */
+    public static Procedure getInstance(Patient patient, Provider provider, String code, String desc, double amount) {
+        return new ProcedureImpl(patient, provider, code, desc, amount);
+    }
 }
